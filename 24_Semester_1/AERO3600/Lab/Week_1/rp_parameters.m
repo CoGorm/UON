1905fd0587a4d1b7fe6328eb49990c242321e89a
CoGorm <cody.gorman97@gmail.com>
2024-02-28 21:27:55 +1100

    parameters

--- 24_Semester_1/AERO3600/Lab/Week_1/rp_parameters.m
+++ 24_Semester_1/AERO3600/Lab/Week_1/rp_parameters.m
@@ -1,5 +1,15 @@
 function rp_p = rp_parameters()
 
+rp_p.mr = 0.095;
+rp_p.Lr = 0.085;
+rp_p.Jr = 1/12;
 
+rp_p.mp = 0.024;
+rp_p.Lp = 0.129;
+rp_p.Jp = 1/12;
+rp_p.g = 9.81;
+
+rp_p.Rm = 8.4;
+rp_p.km = 0.042;
 
 end
